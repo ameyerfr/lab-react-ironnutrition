2238865bb7992b0f38bc815bc57908ccc25fc5cc
Alex Meyer <alexandre.meyer78@gmail.com>
2020-02-19 17:25:51 +0100

    Initial setup & FoodBox

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import FoodBox from './components/FoodBox';
+import 'bulma/css/bulma.css';
 import './App.css';
+
+import foodList from './foods.json';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="root" className="App">
+        <FoodBox food={foodList[0]} />
       </div>
     );
   }
